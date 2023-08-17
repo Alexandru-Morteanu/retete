ece5c936aee9ed5a4ab1289d39e20263856fe8bc
AlexMrt <57711788+Alexandru-Morteanu@users.noreply.github.com>
2023-08-17 20:43:57 +0100

    delete

--- foodguidly/src/app/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-const GreenFoodLoading = () => {
-  return (
-    <div className="loading-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-};
-
-export default GreenFoodLoading;
