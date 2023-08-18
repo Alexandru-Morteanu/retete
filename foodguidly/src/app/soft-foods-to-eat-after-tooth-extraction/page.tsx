3df2772b3cbbae7901275d3286ebc2670a9a2dbe
Alexandru-Morteanu <clanzigzager@gmail.com>
2023-08-18 20:30:38 +0100

    metadata

--- foodguidly/src/app/soft-foods-to-eat-after-tooth-extraction/page.tsx
+++ foodguidly/src/app/soft-foods-to-eat-after-tooth-extraction/page.tsx
@@ -2,32 +2,34 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import BeforeFooter from "../components/BeforeFooter";
 import Article from "../components/Article";
-import Head from "next/head";
-const Soft: React.FC = () => {
-  return (
-    <div
-      style={{
-        paddingTop: 150,
-      }}
-    >
-      <Head>
-        <title>50 soft foods after tooth extraction</title>
-      </Head>
-      <Navigation />
-      <div className="flex items-center flex-col">
-        <div
-          className="flex justify-center items-center"
-          style={{
-            maxWidth: 1182,
-          }}
-        >
-          <Article />
-        </div>
-      </div>
-      <BeforeFooter />
-      <Footer />
-    </div>
-  );
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "50 Soft Foods After Tooth Extraction",
+  description:
+    "Discover 50 delicious and nutritious soft foods for a comfortable recovery after tooth extraction.",
 };
-
-export default Soft;
+export default function Home() {
+  return (
+    <>
+      <div
+        style={{
+          paddingTop: 150,
+        }}
+      >
+        <Navigation />
+        <div className="flex items-center flex-col">
+          <div
+            className="flex justify-center items-center"
+            style={{
+              maxWidth: 1182,
+            }}
+          >
+            <Article />
+          </div>
+        </div>
+        <BeforeFooter />
+        <Footer />
+      </div>
+    </>
+  );
+}
